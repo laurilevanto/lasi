4146c941d134e404c48119e50b885f6cd83182df
Lauri Levanto <laurilev@smail.fi>
2013-10-28 11:01:13 +0200

    paketti-sv added

---
+++ tutustu/tarvitset-sv.php
@@ -0,0 +1,32 @@
+<HTML>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+  <meta name="description" content="inledning i glasfusing">
+  <TITLE>Reseguide till glasfusings värld</title>
+ </head>
+	<BODY bgcolor="ffffff" text="000000" link="0000FF" alink=
+"FF0000" vlink="800080">
+   
+<table cols=3>
+<!--- Left column  --->
+<td bgcolor="#e0ffe0" width="200"
+
+
+  <?php include "../navi-sv.html"; ?> <br>
+  &nbsp;&nbsp;&nbsp; <br>
+    <?php include "./navi-del1.html"; ?>
+</td>
+<!---  Text  --->
+<td bgcolor="#ffffff">
+    <?php include "./tarvitset-sv.html"; ?>
+
+<!--- KUVAT --->
+<td bgcolor="#e0ffe0" width="200">
+&nbsp;&nbsp;&nbsp;
+</td>
+
+ 
+</table>
+</body>
+</html>
+
---
+++ tutustu/tervetuloa-sv.php
@@ -0,0 +1,67 @@
+<HTML>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+  <meta name="description" content="inledning i glasfusing">
+  <TITLE>Reseguide till glasfusings värld</title>
+ </head>
+	<BODY bgcolor="ffffff" text="000000" link="0000FF" alink=
+"FF0000" vlink="800080">
+   
+<table cols=3 bgcolor="#e0ffe0">
+
+<td  width="200">
+
+
+  
+   <?php include "../navi/quickmenu-sv.html"; ?>
+<br>
+   
+</td>
+
+<td bgcolor="#ffffff">
+    <?php include "./tervetuloa-sv.html"; ?>
+</td>
+
+
+<td>
+
+<table>
+  <td width="200" bgcolor="#e0ffe0" valign=top rowspan=2> 
+<b>Och vad kan man göra </b><br>
+ <a href="../pics/tutustu-pics/ristikko1.jpg"> 
+ <img src="../pics/tutustu-pics/th-ristikko1.jpg" width="200"> 
+ <br>personliga fat <br> och tallrikar</img> </a>
+ <br>
+  <a href="../pics/tutustu-pics/kellokukat.jpg"> 
+ <img src="../pics/tutustu-pics/kellokukatth.jpg" width="200"> 
+ <br>Olika vaser</img> </a>
+ <br>
+   <a href="../galleries/lauri/drift-leave3.jpeg"> 
+ <img src="../galleries/lauri/drift-leave3.jpeg" width="200"> 
+ <br>smycken</img> </a>
+
+ <a href="../pics/tutustu-pics/madonna.jpg"> 
+ <img src="../pics/tutustu-pics/madonnath.jpg" width="200"> 
+ <br>tavlor</img> </a>
+ <br>
+
+
+  <a href="../pics/tutustu-pics/waterlilies.jpg"> 
+ <img src="../pics/tutustu-pics/waterliliesth.jpg" width="200"> 
+ <br>ljus underlag</img> </a>
+ <br>
+  <br>
+  <a href="../pics/tutustu-pics/lisko.jpg"> 
+ <img src="../pics/tutustu-pics/liskoth.jpg" width="200"> 
+ <br>och även skulpturer</img> </a>
+  </td>
+
+ </table>
+ <!--- END KUVAT --->  
+</table>
+
+
+
+</body>
+</html>
+
