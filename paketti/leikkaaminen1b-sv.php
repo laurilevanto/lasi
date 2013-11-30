5ddc1fa5c8921f2916c7c516167980928aada688
Lauri Levanto <laurilev@smail.fi>
2013-11-30 19:20:10 +0200

    leikkaaminen*

---
+++ paketti/leikkaaminen1b-sv.php
@@ -0,0 +1,115 @@
+<HTML>
+ <?php include "../navi/head.html"; ?>
+	<BODY bgcolor="FFFFFF" text="000000" link="0000FF" alink="FF0000" vlink="800080">
+
+<p>1.6.2</p>
+
+
+<table  COLS=3 WIDTH="100%"  border >
+<td>
+<td> menu
+
+            <?php include "../tutustu/menu1-sv.html"; ?>   
+       
+   </td>
+   </td>
+ 
+ <!---END vasen--->
+ 
+<!---Keskipalsta TEKSTI --->
+
+  <td>
+<table width="600" align="center"  border>
+
+<td>
+   
+   Forts. <br>
+   
+<h2>Att skära glas 2</h2>
+
+ <p>
+<h3> Att skära med linjalen </h3>
+Linjalen har en tendens att glida på glaset. 
+En bit av mjukt material under linjalen hjälper. 
+Jag har användt vanligt rullplåster.
+Kom ihåg när du mäter glaset att skäraren har en tjoklek 
+om 5 mm.
+Spåret kommer till hälften av det, 2,5 mm från linjalen. 
+Det är en smaksak on du skär bårtåt ellet inåt med linjalen
+Ofta är det enklast att börja med den närmaste kanten.
+Se upp att spåret börjar just från kanten. 
+Vid slutkanten drar man över kanten.
+</p>
+<p>
+Besök en glasaffär och be om några bortkastade bitar 
+för att öva med.
+Fönstergas är lättast att skära. 
+Sen kan du pröva vad har du lärt med dyrare konstglas.
+</p>
+Man kan inte skära en rättvinkel bit från glasskivan. 
+Spåret måste gå från kant till kant oavbrutet.
+<p>
+<h3>Att skära med fri hand</h3>
+Böjda linjer måste man skära på fri hand. 
+Dock kan man rita med tusch
+var linjen ska gå. 
+Med klart glas kan man skära på teckningen som syns
+genom glaset.</p>
+<p>
+När man planerar böjda linjer måste man komma ihåg 
+att slutligen öppnas spåret med att böja glas.
+Öppningen har en tendess att gå vilse till utkanten.
+ Även böjda linjer måste skäras från kant till kant.
+ Sen kan man fortsätta med annu lite närmare 
+ det slutliga mönstret.
+</p>
+
+<p>
+Branta insida kurvor är nästan omöjligt att skära. 
+Oftast måste man göra det stegvist
+med arr börja med mindre kurvor.
+<p>
+
+</td>
+</td>
+<td
+<!--- END TEKSTI --->
+    <!--- oikealle KUVAT --->
+<table>
+  <td > &nbsp;&nbsp;&nbsp; </td>
+
+   <
+ <!--- END KUVAT --->  
+ </table>
+ </td>
+ 
+   <!--- ALALINKIT --->
+<table  cols=3  width=100% bgcolor=e0ffe0>
+<tr>
+  <td  >
+
+    takaisin <A href="../index-sv.html"> Framsida</A> 
+    </td>
+    <td>
+    <?php include "../navi/copy2.html"; ?>
+    </td>
+    <td>
+    <p align=right> 
+ <a href="../paketti/leikkaaminen2-sv.php"> fortsätter -></a>  
+  </td>
+  </tr>
+</table>
+<!--- END-ALALINKIT --->
+
+ 
+
+
+</body>
+</html>
+
+
+
+
+
+
+
---
+++ paketti/leikkaaminen2-sv.php
@@ -0,0 +1,136 @@
+<html>
+<p>1.6.3</p>
+<table  COLS=3 WIDTH="100%"  border >
+<td>
+<td> 
+
+            <?php include "../tutustu/menu1-sv.html"; ?>   
+       
+   </td>
+  </td>
+ 
+ <!---END vasen--->
+ 
+ <td>
+<table border width="600" >
+   <td>
+<h3>Att öppna skåran </h3>
+<p> 
+Efter skärning måste man öppna skåran.
+<p>
+1. Det säkraste sätt är att vända glaset med skåran  nedåt
+ på ett litet flexibel underlag, och sen trycka med tummen 
+ så att skåran öppnas genon hela skivans tjoklek.
+ Det kan man göra endast med transparent glas 
+ där man kan se genom var skåran är. 
+ Även där är en fördel om man har ritat linjen med tusch.
+ </p>
+ <p>
+2. den andra metoden är att man böjer glaset bortifrån 
+skåran. 
+Då spricker det efter linjen.
+ </p>
+ <p>
+3. En lång rak linje är lättast att öppna med att föra skåran till bordkanten.
+Med ena handen håller man glaset på plats mot bordet 
+och med den andra handen tar man ett grepp på kanten 
+så att biten kan inte falla på golvet. 
+Litet tryck med den överhängande del oppnar skåran.
+Låter skrammande men är mycket lätt.
+Kom dock ihåg att använda skyddhanskar.
+</p>
+<p>
+4.  En mycket lång linje mitt på glaset är lättast att öppna med 
+att man sätter en penna   vid glaskanten under skåran.
+Sen sätter man handflaten på bägge sidor av 
+skåran och trycker ned.
+Hela längden av skåran öppnas samtidigt.
+</p>
+<p>
+5. Mindre glasbitar kan man bryta i händer. Tummen kommer 
+vid skårans sidor och pekfingrar stöder underfrån.
+</p>
+ .
+<p>
+6. När man vill skära smala strimlor av glaskanten är de svåra att bryta av.
+Då andvänder man bryttång.
+</p>
+<p>
+7. En variant av tekniken 1. är att man tar en smal skruvmejsel och 
+med plaster eller isoleringsband mjuknar huvudet. Man sätter mejseln
+på baksidan, jämnt mot skåran och slår med handflaten på mejseln huvud.
+</p>
+8. I ytterst nödfall kan man försöka med att lätt knacka till skråmas baksida
+med glasskärarens metalhuvud. 
+Den metoden är vanlig i tiffanyarbetet men ger alrig en nätt kant.
+Brytlinjen blir snedt med farligt skarpa kanter.
+I tiffanyarbeten slipar man kanten ändå.
+<p>
+tt annat nöd trick är att göra en ny skråma  på andra sidan av glasskivan,
+Oftast är det säkrast att ge opp och göra ett nytt försök  annastans.
+<p>
+Muita aiheeseen liittyviä sivuja
+<table border bgcolor ="#e0ffe0">
+<tr>
+  <td><a href="../tarv/lasiveitsi1.php"> Lasiveitsi</a> </td>
+</tr>
+<tr>
+  <td><a href="../tarv/pihdit.php"> Aukaisupihdit</a> </td>
+</tr>
+<tr>
+  <td><a href="../tekniikat/leikkautuminen.php"> Lasin leikkaaminen</a> </td>
+</tr>
+<tr>
+  <td> <a href="../tekniikat/leikkaus.php"> Viirun vetäminen</a></td>
+</tr>
+<tr>
+  <td> <a href="../tekniikat/aukaisu.php"> Railon aukaisu </a></td>
+</tr>
+<tr>
+  <td> <a href="../polku/kysymys.html"> Pullon katkaisu</a></td>
+</tr>
+<tr>
+  <td> <a href="../polku/kysymys.html#reunanhionta"> hiominen</a></td>
+</tr>
+
+</table>
+</p>
+<p>
+
+  <!--- ALALINKIT --->
+<table  cols=3  width=100% bgcolor=e0ffe0>
+<tr>
+  <td  >
+
+    takaisin <A href="../index-sv.html"> Framsida</A> 
+    </td>
+    <td>
+    <?php include "../navi/copy2.html"; ?>
+    </td>
+    <td>
+    <p align=right> Nästa sida
+ <a href="./sulatuslasi-sv.php"> Glas för fusing -></a>  
+  </td>
+  </tr>
+</table>
+<!--- END-ALALINKIT --->
+
+ 
+</td>
+</table>
+</td>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
