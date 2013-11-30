188d1b959d4a4a68be2ef40897c3e4ab1625405a
Lauri Levanto <laurilev@smail.fi>
2013-11-30 19:18:22 +0200

    leikkaaminen

---
+++ paketti/leikkaaminen1-sv.php
@@ -0,0 +1,170 @@
+<HTML>
+ <?php include "../navi/head.html"; ?>
+	<BODY bgcolor="FFFFFF" text="000000" link="0000FF" alink="FF0000" vlink="800080">
+
+
+<p>1.6.1</p>
+
+<table  COLS=3 WIDTH="100%"  border >
+<td>
+<td> menu
+
+            <?php include "../tutustu/menu1-sv.html"; ?>   
+       
+   </td>
+   </td>
+ 
+ <!---END vasen--->
+ 
+<!---Keskipalsta TEKSTI --->
+
+  <td>
+<table width="600" align="center"  border>
+
+<td>
+   
+   
+<h2>Att skära glas 1</h2>
+<table border>
+<td>
+<font color="#FF0000">
+Använd ögonskydd <strong>alltid </strong>
+när du skär eller slipar glas</font>
+</td>
+</table>
+
+Glas är hårt amorfisk material som har stor ytspänning.
+När man bryter ytspänningen från ena sidan 
+med att skära en linje med glasskäraren,
+bryts glaset lätt enligt linjen, när det böjs bort från linjen
+<p>
+För att välja en glasskärare har jag bara ett råd. 
+Skäraren är bra om den känns bekväm att använda.
+Dock skall man välja sin skärare i en högre prisklass. 
+Avgörande del är trissan som då är av bättre kvalitet.
+De billigaste skärare har ett revolverhuvud med många trissor. 
+Det berättar att även fabriken tror 
+att den första trissan håller inte långt.
+En bra trissa tål att dra flera kilometer linje.
+</p>
+<p>
+Jag har märkt att olika människor har sina favorit märken.
+Greppet på skäraren varierar.  
+Det finns skärare med rakt eller böjt skaft.
+Om man håller ett rakt skaft med samma grepp som en penna 
+kan man lätt styra linjen.
+När ett böjt skaft ligger mot handen är det lättare 
+att få rätt tryck.
+Det har ochså betydelse om man föredrar att skära mot sig 
+eller bortåt.
+Med ett böjt skaft är det lättast at skära bortåt, 
+då ser man var skäraren går.
+Med ett rakt skaft kan man skärä också inåt.
+</p>
+<p>
+Det bästa underlaget är jämn och litet mjuk. 
+En tunn gummimatta är bra.
+En gammal handduk eller  ett par lager av gamla tidningar 
+duger bra.
+Tidningspappret har den fördel att glasskärvor 
+går till avfall med pappret.
+</p>
+<p>
+Akta för att det inte finns gamla skärvor 
+eller annat ojämnhet under glasskivan.
+Då kan skärarens tryck bryta glaset i en oönskad riktning. 
+Bordet kan gärna vara ganska lågt.
+
+</p>
+<p>
+Idén är att få trycket med kroppen, inte med handmuskeln.
+Stöd övre armen mot kroppen ocl sätt bena efter varann.
+Rörelsen sker med att flytta kroppvikt med ben, 
+mindre med arm.
+Vid behov kan du stöda skäraren sidlängs med den andra hand.
+</p>
+<p>
+Man ska skära med ett långt drag från ena kanten til den andra. 
+Det behöver dock inte vara snabbt.
+För snabbt  drag kan få skäraren att hoppa 
+om glasytan är ojämn.
+Då får du inte ett kontinuerligt spår.
+</p>
+Du har rätt trycknär du hör ett svagt ljud. 
+Ett bra spår är knappt synlig, 
+man knappast känner det när man drar fingernageln tvärs. 
+Om spåret har synligt ojämhet har du tryck för hårt.
+
+ <p>
+ Man måste öva intill att spåren lyckas hela vägen 
+ med ett försök.
+ Man ska inte försöka dra  ett nytt spår på det gamla. 
+ Det ödelägger glasskäraren snabbt
+ och gör det ännu svårare att bryta spåret opp.
+
+ 
+</td>
+Muita aiheeseen liittyviä sivuja
+<table border bgcolor ="#e0ffe0">
+<tr>
+  <td><a href="../tarv/lasiveitsi1.php"> Lasiveitsi</a> </td>
+</tr>
+<tr>
+  <td> <a href="../tarv/leikkausoljy.php"> Lasinleikkausöljy</a></td>
+</tr>
+<tr>
+  <td><a href="../tekniikat/leikkautuminen.php"> Lasin leikkaaminen</a> </td>
+</tr>
+<tr>
+  <td> <a href="../tekniikat/leikkaus.php"> Viirun vetäminen</a></td>
+</tr>
+<tr>
+  <td> <a href="../tekniikat/aukaisu.php"> Railon aukaisu </a></td>
+</tr>
+</table>
+</td>
+<td
+<!--- END TEKSTI --->
+    <!--- oikealle KUVAT --->
+<table>
+  <td width="200" bgcolor="#e0ffe0" valign=top rowspan=2> 
+
+   <a  href=../pics/tarv-pics/lasiveitset.jpg>
+     <img src=../pics/tarv-pics/lasiveitsetth.jpg width="190"> <br>
+    glas skärare<br>
+    <font size="-2">(foto: Lasipaja Väisälä)</font></a>
+ 
+ <!--- END KUVAT --->  
+ </table>
+ </td>
+ 
+   <!--- ALALINKIT --->
+<table  cols=3  width=100% bgcolor=e0ffe0>
+<tr>
+  <td  >
+
+    takaisin <A href="../index-sv.html"> Framsida</A> 
+    </td>
+    <td>
+    <?php include "../navi/copy2.html"; ?>
+    </td>
+    <td>
+    <p align=right> 
+ <a href="./leikkaaminen1b-sv.php"> fortsätter -></a>  
+  </td>
+  </tr>
+</table>
+<!--- END-ALALINKIT --->
+
+ 
+
+
+</body>
+</html>
+
+
+
+
+
+
+
