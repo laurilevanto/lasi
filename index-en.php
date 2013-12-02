4286397020413608159ac7e3fb77b599c0410b10
Lauri Levanto <laurilev@smail.fi>
2013-12-02 12:20:43 +0200

    cleanup

---
+++ index-en.php
@@ -0,0 +1,99 @@
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+  <meta name="description" content="Lasinsulatus opas">
+  <TITLE>Lasinsulatus</title>
+ </head>
+	<BODY bgcolor="ffffff" text="000000" link="0000FF" alink=
+"FF0000" vlink="800080">
+<table>
+<tr>
+  <td> page 1.</td>
+</tr>
+ <tr>
+     <td ><strong><font size="+1">Lauri Levanto</font></strong></td>
+      <td align=right>&nbsp;&nbsp;<em>lauri</em></td><td>@</td> <td><em>lsd.dk</em></td> 
+     </td>
+   </tr>    
+
+ </table>
+ 
+    <h1 > Guide Book to Glass Fusing </h1>
+<table cols="2" width="100%">
+   
+    <td >
+
+ </tr>
+</table>
+</td>
+    <td> 
+    <table>
+  
+<table cols =2 width=100% >
+<tr>
+
+  <td>
+      <?php include "./navi/navi-en.html"; ?>
+  </td>
+  
+  <td>
+ 
+ 
+    <i><font size="+1"> Glass fusing is forming of glass in a glass or ceramic kiln
+    </font></i><br>
+   <p> The purpose of this site is to guide you into fusing.
+   The topic is fascinating but wide.
+   Everyone must find her/his path according to her/his own interests
+   I have tried to ease the hinders a beginner will meet, 
+   but only your own experience can smoothen the path
+   
+   I have included road signs (<i>links</i>) to particular topics or external sites
+   in order to guide you to deeper understanding or to some particular sight
+   
+   
+ </p> The site has two parts<br>
+   <a href="./tutustu/lukijalle.php"><b>PART 1 introduction tour </b></a>
+    Only in Finnish<br>
+     <a href="./navi/osio2-navi.html"><b>   <b>PART 2 Maps</b></b></a>   
+    Mainly in Finnish, some bilingual pages
+    <br>
+        <a href="./english.html"><b>Start here</b></a> 
+        </td>
+ 
+    <td> <td width=30% bgcolor="#ffffff" align=center >
+  <a href="./valk-tuikku.jpg"> 
+ <img src="./valk-tuikku.jpg" alt="" width=250 />  <br> 
+  <strong>Talven valoa </strong><em><strong>Winter Light</strong>
+  </em><br>
+  </img>
+  </a>
+   <br>
+</td>
+</tr>
+</table>
+<br>
+  <div id="google_translate_element"></div>
+  <script type="text/javascript">
+function googleTranslateElementInit()
+ {  new google.translate.TranslateElement({pageLanguage: 'fi', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL, multilanguagePage: true}, 'google_translate_element');}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<table border bgcolor="#ffffe0" align=center width=80%>
+<td>
+<font size="-2"><strong>Copyright 2007,2011 Lauri Levanto</strong> &nbsp;&nbsp;
+<strong> URL http://lauri.lsd.dk/lasi/</strong><br>
+ Yöu have permission to copy in data or in print with conditin that
+ the copyright statement in that page is included. <br>
+ If a picture or text element has reference to the artist or author
+ you have to ask separate permission from that copyright owner.
+  <br>
+  The works in the pictures are intellectual property of the maker and yhose you are not 
+  permitted to produce copies for sale. The techniques are not protected and you can freely apply them in your own work.
+
+
+</font>
+</td>
+</table>
+
+</body>
+</html>
---
+++ index-sv.php
@@ -0,0 +1,32 @@
+<HTML>
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+  <meta name="description" content="Lasinsulatus opas">
+  <TITLE>Glasfusing</title>
+  <meta name="google-translate-customization" content="69be97c55ee6b234-f0c018964106e741-gdbfaef1f9de9036f-12"></meta>
+ </head>
+	<BODY bgcolor="ffffff" text="000000" link="0000FF" alink=
+"FF0000" vlink="800080">
+  <table cols="3" width="100%" border bgcolor="#a0e0a0">
+  <tr>
+    <td >
+    <a href="./tutustu/lukijalle.php"><img src="flag-fin.jpg" width=150></img><b>Suomeksi</b> </a>
+    </td>
+     <td >
+    <td >
+    <a href="./tutustu/lukijalle-sv.php"><img src="flag-sv.jpg" width=150></img><b>På Svenska</b></a>
+    </td>
+     <td >
+    <a href="./english.html"><img src="flag-eng.jpg" width=150></img><b>In English</b></a>
+    </td>
+    </tr>
+ <table>
+
+  <?php include "./index-sv.html"; ?>
+  <br>
+ 
+ 
+   </body>
+</html>
+
