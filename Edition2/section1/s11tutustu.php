43b839bf6dcd434240e6e021f777c03a58d10bec
Lauri Levanto <laurilev@smail.fi>
2014-10-06 12:30:45 +0300

    section1 tutustu  Please enter the commit message for your changes. Lines starting

---
+++ Edition2/section1/s11tutustu.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+  <meta name="description" content="Lasinsulatus opas">
+  <TITLE>Lasinsulatus</title>
+ </head>
+ <body>
+   <?php include "../topmenu.html"; ?>
+   <h3>1.Esittely</h3>
+ <table>
+ <td>
+ <p>
+ Tämä jakso on tarkoitettu luettavaksi yhtenäisenä järjestyksessä. 
+ Sivujen oikeassa alakulmassa on siksi linkki seuraavalle sivulle. 
+ </p> <p>
+ <h3>1.1 Miten pääsen alkuun</h3>
+ Ehdottomasti paras tapa on käydä joku kurssi. 
+ Niitä järjestävät kansalais- ja työväenopistot sekä eräät taidelasi-lliikkeet. 
+ Lyhyt viikonlopun kurssikin on hyvä alku. 
+ Kokemus tiffanytyöstä ja jossain määrin keramiikasta on avuksi 
+ mutta ei välttämätöntä. 
+ Jos on pakko aloittaa omin päin, toivon näistä sivuista olevan apua. 
+ Joka tapauksessa vain oma kokeilu ja kokemus on tärkeää. 
+ Eihän kukaan opi polkupyörällä ajamistakaan lukemalla. 
+  </p> 
+  </td>
+  <td>
+  <p>
+ Lasinsulatus tarjoaa monia mahdollisuuksia kotoisesta askartelusta 
+ luovaan lasitaiteeseen, ja jopa itsenäiseksi yrittäjäksi. 
+ Tässä sivustossa on myös kysymys ja vastaus   -palsta.  Älä epäröi kysyä. 
+ Kaikki kysymykset ovat tervetulleita, ja vastauksista on sitten apua 
+ muillekin samaan ongelmaan törmänneille. Osoite on kansilehdellä.
+ </td>
+ </table>
+
+</p>
+
+<table width="1000" bgcolor="a0a0ff">
+<tr>
+<td align="left"> <a href="../../index.php">Etusivu </a></td> 
+<td align="center"> Copyright 2014 Lauri Levanto</td> 
+<td align="right"> <a href="./s1mitatehda.php">
+Seurava sivu </a></td>
+</tr>
+</table>
+</body>
+</html>
