c91673997dc38de4a66db62e3cf531482286dc8f
Heikki Levanto <heikki@idmail.dk>
2018-02-21 21:40:38 +0100

    Added the rip page that I had forgotten to commit in 2014

---
+++ pages/rip.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta name="description" content="Lasinsulatus opas">
+  <TITLE>Lasinsulatus</title>
+ </head>
+<BODY bgcolor="ffffff" text="000000" link="0000FF" alink="FF0000" vlink="800080">
+
+<h2>Lauri Levanto nukkui pois 30.10.2014</h2>
+
+Meni illalla nukkumaan, ja aamulla ei sitten herännytkään. Paljon
+jäi kesken. <p/>
+
+Lauri kirjoitteli <a href="http://blogimetsa.fi">blogimetsä</a> sivustolle.
+Lauria muistellaan siellä 
+<a href="http://blogimetsa.fi/item/1614-kiitos-lauri-levanto">tuossa</a> ja 
+<a href="http://blogimetsa.fi/item/1618-laurin-muistelua">tuossa</a> ja 
+muuallakin.
+<p/>
+Lauri keskusteli lasin tekemisestä 
+<a href="http://warmglass.com">warmglass.com</a> sivuilla.
+Siellä häntä muistellaan
+<a href="http://www.warmglass.com/phpBB/viewtopic.php?f=19&t=41734">tässä</a>.
+<p/>
+Laurin omat sivut ovat <a
+href="http://lauri.lsd.dk/lasi/">http://lauri.lsd.dk/lasi/</a>
+<p/>
+Heikki Levanto, <br/>
+Laurin poika <br/>
+<i>heikki</i>(a)<i>lsd.dk</i>
+<p/>
+<hr/>
+
+<h2>Lauri Levanto passed away 30 Oct 2014 </h2>
+
+Lauri was active at 
+<a href="http://warmglass.com">warmglass.com</a>, discussing glass works.
+People are remembering him at
+<a href="http://www.warmglass.com/phpBB/viewtopic.php?f=19&t=41734">this</a>
+page.
+<p/>
+Lauris wrote quite large set of pages about glass at
+ <a href="http://lauri.lsd.dk/lasi/">http://lauri.lsd.dk/lasi/</a>. Mostly 
+in Finnish, but some pages are also in English and Swedish.
+<p/>
+
+Heikki Levanto <br/>
+Lauri's son <br/>
+<i>heikki</i>(a)<i>lsd.dk</i>
+
+
